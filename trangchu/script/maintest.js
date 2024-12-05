a50d55174ad6a9580ff81e568782f1a4cecd09ca
Huy Ma <huymd204@gmail.com>
2024-12-05 14:22:11 +0700

    update images folder nole

--- trangchu/script/maintest.js
+++ trangchu/script/maintest.js
@@ -1,23 +1,135 @@
 // Danh sách vật phẩm
 const items = [
-    { name: "Nô lệ Hiếu Bùi", type: "nole", img: "nole/bhieu.jpg", gia: 10, increment: 1, unit: "Tiền mỗi giây", quantity: 0 },
-    { name: "Gậy đánh chó", type: "tool", img: "tools/gaydanhcho.png", gia: 10, increment: 1, unit: "Tiền mỗi click", quantity: 0 },
-    { name: "Nô lệ Trung Bùi", type: "nole", img: "nole/trung.jpg", gia: 500, increment: 10, unit: "Tiền mỗi giây", quantity: 0 },
-    { name: "Búa gõ đầu", type: "tool", img: "tools/buagodau.png", gia: 100, increment: 10, unit: "Tiền mỗi click", quantity: 0 },
-    { name: "Nô lệ Vinh đần", type: "nole", img: "nole/vinh.jpg", gia: 5000, increment: 100, unit: "Tiền mỗi giây", quantity: 0 },
-    { name: "Kiếm cùn", type: "tool", img: "tools/kiemcun.png", gia: 1000, increment: 100, unit: "Tiền mỗi click", quantity: 0 },
-    { name: "Nô lệ Huy", type: "nole", img: "nole/huy.jpg", gia: 50000, increment: 1000, unit: "Tiền mỗi giây", quantity: 0 },
-    { name: "Rìu cứu hỏa", type: "tool", img: "tools/riucuuhoa.jpg", gia: 10000, increment: 1000, unit: "Tiền mỗi click", quantity: 0 },
-    { name: "Nô lệ Khoa", type: "nole", img: "nole/khoa.jpg", gia: 500000, increment: 10000, unit: "Tiền mỗi giây", quantity: 0 },
-    { name: "Lightsaber", type: "tool", img: "tools/kiemanhsang.png", gia: 100000, increment: 10000, unit: "Tiền mỗi click", quantity: 0 },
-    { name: "Nô lệ Hào", type: "nole", img: "nole/hao.jpg", gia: 5000000, increment: 100000, unit: "Tiền mỗi giây", quantity: 0 },
-    { name: "Kiếm thánh Excalibur", type: "tool", img: "tools/excalibur.png", gia: 1000000, increment: 100000, unit: "Tiền mỗi click", quantity: 0 }
+    {
+        name: 'Nô lệ Hiếu Bùi',
+        type: 'nole',
+        img: 'nole/nft01.avif',
+        gia: 10,
+        increment: 1,
+        unit: 'Tiền mỗi giây',
+        quantity: 0
+    },
+    {
+        name: 'Gậy đánh chó',
+        type: 'tool',
+        img: 'nole/nft02.jpg',
+        gia: 10,
+        increment: 1,
+        unit: 'Tiền mỗi click',
+        quantity: 0
+    },
+    {
+        name: 'Nô lệ Trung Bùi',
+        type: 'nole',
+        img: 'nole/nft03.avif',
+        gia: 500,
+        increment: 10,
+        unit: 'Tiền mỗi giây',
+        quantity: 0
+    },
+    {
+        name: 'Búa gõ đầu',
+        type: 'tool',
+        img: 'nole/nft04.jpg',
+        gia: 100,
+        increment: 10,
+        unit: 'Tiền mỗi click',
+        quantity: 0
+    },
+    {
+        name: 'Nô lệ Vinh đần',
+        type: 'nole',
+        img: 'nole/nft05.jpg',
+        gia: 5000,
+        increment: 100,
+        unit: 'Tiền mỗi giây',
+        quantity: 0
+    },
+    {
+        name: 'Kiếm cùn',
+        type: 'tool',
+        img: 'nole/nft06.jpg',
+        gia: 1000,
+        increment: 100,
+        unit: 'Tiền mỗi click',
+        quantity: 0
+    },
+    {
+        name: 'Nô lệ Huy',
+        type: 'nole',
+        img: 'nole/nft07.jpg',
+        gia: 50000,
+        increment: 1000,
+        unit: 'Tiền mỗi giây',
+        quantity: 0
+    },
+    {
+        name: 'Rìu cứu hỏa',
+        type: 'tool',
+        img: 'nole/nft08.avif',
+        gia: 10000,
+        increment: 1000,
+        unit: 'Tiền mỗi click',
+        quantity: 0
+    },
+    {
+        name: 'Nô lệ Khoa',
+        type: 'nole',
+        img: 'nole/nft09.jpg',
+        gia: 500000,
+        increment: 10000,
+        unit: 'Tiền mỗi giây',
+        quantity: 0
+    },
+    {
+        name: 'Lightsaber',
+        type: 'tool',
+        img: 'nole/nft10.jpg',
+        gia: 100000,
+        increment: 10000,
+        unit: 'Tiền mỗi click',
+        quantity: 0
+    },
+    {
+        name: 'Nô lệ Hào',
+        type: 'nole',
+        img: 'nole/nft11.jpg',
+        gia: 5000000,
+        increment: 100000,
+        unit: 'Tiền mỗi giây',
+        quantity: 0
+    },
+    {
+        name: 'Kiếm thánh Excalibur',
+        type: 'tool',
+        img: 'nole/nft12.jpg',
+        gia: 1000000,
+        increment: 100000,
+        unit: 'Tiền mỗi click',
+        quantity: 0
+    }
 ];
 
 const rareitems = [
-    { name: "Thánh dược Elixir", type: "elixir", img: "tools/exilir.png", increment: 2, unit: "Tiền mỗi giây", percent: 50, quantity: 0 },
-    { name: "Thiên thạch", type: "grindstone", img: "tools/damai.png", increment: 2, unit: "Tiền mỗi click", percent: 50, quantity: 0 }
-]
+    {
+        name: 'Thánh dược Elixir',
+        type: 'elixir',
+        img: 'tools/exilir.png',
+        increment: 2,
+        unit: 'Tiền mỗi giây',
+        percent: 50,
+        quantity: 0
+    },
+    {
+        name: 'Thiên thạch',
+        type: 'grindstone',
+        img: 'tools/damai.png',
+        increment: 2,
+        unit: 'Tiền mỗi click',
+        percent: 50,
+        quantity: 0
+    }
+];
 let hasElixir = false; // Kiểm tra nếu Elixir đã có trong rương
 let hasGrindstone = false; // Kiểm tra nếu Grindstone đã có trong rương
 let money = 500;
@@ -50,8 +162,6 @@ function renderItems() {
 
         container.appendChild(itemHTML); // Thêm phần tử vào container
     });
-
-
 }
 function renderRareItems() {
     const container = document.getElementById('rareitemsContainer'); // Lấy container chính
@@ -72,8 +182,6 @@ function renderRareItems() {
 
         container.appendChild(itemHTML); // Thêm phần tử vào container
     });
-
-
 }
 // Mua vật phẩm
 function buyItem(index) {
@@ -82,25 +190,29 @@ function buyItem(index) {
         money -= item.gia;
         item.quantity += 1;
         item.gia = Math.floor(item.gia * 1.5); // Tăng giá sau mỗi lần mua
-        if (item.type === "nole") moneyPerSecond += item.increment;
-        if (item.type === "tool") moneyPerClick += item.increment;
+        if (item.type === 'nole') moneyPerSecond += item.increment;
+        if (item.type === 'tool') moneyPerClick += item.increment;
 
         // Cập nhật giao diện
-        document.getElementById(`soluong${item.type}${index}`).textContent = `${item.name}: ${item.quantity}`;
-        document.getElementById(`gia${item.type}${index}`).textContent = `Giá: ${item.gia} VNĐ`;
+        document.getElementById(
+            `soluong${item.type}${index}`
+        ).textContent = `${item.name}: ${item.quantity}`;
+        document.getElementById(
+            `gia${item.type}${index}`
+        ).textContent = `Giá: ${item.gia} VNĐ`;
         updateDisplay();
     } else {
-        alert("Không đủ tiền!");
+        alert('Không đủ tiền!');
     }
 }
-
 
 // Mở rương
 function openChest() {
     // Kiểm tra nếu người chơi có đủ tiền và cả 2 vật phẩm chưa có
-    if (money >= 500) { // Giả sử mỗi lần mở rương tốn 500 VNĐ
+    if (money >= 500) {
+        // Giả sử mỗi lần mở rương tốn 500 VNĐ
         if (hasElixir && hasGrindstone) {
-            alert("Rương đã chứa đủ vật phẩm, không thể mở nữa!");
+            alert('Rương đã chứa đủ vật phẩm, không thể mở nữa!');
             return; // Nếu cả 2 vật phẩm đã có, không thể mở rương
         }
 
@@ -111,14 +223,18 @@ function openChest() {
         const selectedItem = getRandomItem();
 
         // Cập nhật trạng thái vật phẩm đã có trong rương
-        if (selectedItem.name === "Thánh dược Elixir") {
+        if (selectedItem.name === 'Thánh dược Elixir') {
             hasElixir = true;
             rareitems[0].quantity += 1;
-            document.getElementById(`soluongelixir0`).textContent = `Thánh dược Elixir: ${rareitems[0].quantity}`;
-        } else if (selectedItem.name === "Thiên thạch") {
+            document.getElementById(
+                `soluongelixir0`
+            ).textContent = `Thánh dược Elixir: ${rareitems[0].quantity}`;
+        } else if (selectedItem.name === 'Thiên thạch') {
             hasGrindstone = true;
             rareitems[1].quantity += 1;
-            document.getElementById(`soluonggrindstone1`).textContent = `Thiên thạch: ${rareitems[1].quantity}`;
+            document.getElementById(
+                `soluonggrindstone1`
+            ).textContent = `Thiên thạch: ${rareitems[1].quantity}`;
         }
 
         // Cập nhật kết quả vật phẩm nhận được
@@ -129,7 +245,7 @@ function openChest() {
 
         updateDisplay(); // Cập nhật lại giao diện
     } else {
-        alert("Không đủ tiền để mở rương!");
+        alert('Không đủ tiền để mở rương!');
     }
 }
 
@@ -137,36 +253,34 @@ function openChest() {
 function getRandomItem() {
     // Nếu đã có Elixir, chỉ chọn Thiên thạch
     if (hasElixir) {
-        return rareitems.find(item => item.name === "Thiên thạch");
+        return rareitems.find((item) => item.name === 'Thiên thạch');
     }
     // Nếu đã có Thiên thạch, chỉ chọn Elixir
     else if (hasGrindstone) {
-        return rareitems.find(item => item.name === "Thánh dược Elixir");
+        return rareitems.find((item) => item.name === 'Thánh dược Elixir');
     }
     // Nếu chưa có vật phẩm nào, chọn ngẫu nhiên với tỉ lệ 50% cho mỗi vật phẩm
     else {
         const random = Math.random();
         if (random < 0.5) {
-            return rareitems.find(item => item.name === "Thánh dược Elixir");
+            return rareitems.find((item) => item.name === 'Thánh dược Elixir');
         } else {
-            return rareitems.find(item => item.name === "Thiên thạch");
+            return rareitems.find((item) => item.name === 'Thiên thạch');
         }
     }
 }
 
-
 // Hàm áp dụng tác dụng của vật phẩm
 function applyItemEffect(selectedItem) {
-    if (selectedItem.type === "elixir") {
+    if (selectedItem.type === 'elixir') {
         mpsMultiplier = mpsMultiplier * 2;
-    } else if (selectedItem.type === "grindstone") {
+    } else if (selectedItem.type === 'grindstone') {
         mpcMultiplier = mpcMultiplier * 2;
     }
 }
 
-
 // Click để nhận tiền
-document.getElementById("clickMeButton").addEventListener("click", () => {
+document.getElementById('clickMeButton').addEventListener('click', () => {
     money += moneyPerClick * mpcMultiplier;
     updateDisplay();
 });
@@ -179,9 +293,13 @@ setInterval(() => {
 
 // Cập nhật hiển thị
 function updateDisplay() {
-    document.getElementById("money").textContent = `Tiền: ${money} VNĐ`;
-    document.getElementById("mpc").textContent = `Tiền mỗi click: ${moneyPerClick * mpcMultiplier}`;
-    document.getElementById("mps").textContent = `Tiền mỗi giây: ${moneyPerSecond * mpsMultiplier}`;
+    document.getElementById('money').textContent = `Tiền: ${money} VNĐ`;
+    document.getElementById('mpc').textContent = `Tiền mỗi click: ${
+        moneyPerClick * mpcMultiplier
+    }`;
+    document.getElementById('mps').textContent = `Tiền mỗi giây: ${
+        moneyPerSecond * mpsMultiplier
+    }`;
 }
 
 // Khởi tạo
